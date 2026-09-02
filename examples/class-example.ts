fc24109bfcb6779e77d1300695bd6098a72717de
user <user@staryoga>
2026-09-02 15:46:22 +0200

    updated example

--- examples/class-example.ts
+++ examples/class-example.ts
@@ -9,7 +9,13 @@ function fn1(user: User) {
 	console.log(user);
 }
 
+function fn2(user: User) {
+	console.log(user);
+}
+
 second();
 fn1(new User());
+fn2('other type');
+second();
 
 fn1('other type'); // TypeError: user must be User
